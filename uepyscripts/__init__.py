86db319c1d74ca8496315f0a5ce94d6dee413de6
Michael Delva <michael.delva@fishingcactus.com>
2025-07-25 13:49:10 +0200

    Changed logging to INFO

--- uepyscripts/__init__.py
+++ uepyscripts/__init__.py
@@ -1,12 +1,12 @@
 import logging
 
 logger = logging.getLogger("uepyscripts")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
