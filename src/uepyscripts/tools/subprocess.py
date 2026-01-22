285b6d17e30b7768e2dbea497acd73fb73a726ca
Michael Delva <michael@emidee.net>
2026-01-22 15:49:27 +0100

    Fixed subprocess.run_subprocess to not wait for the process when log_output is false

--- src/uepyscripts/tools/subprocess.py
+++ src/uepyscripts/tools/subprocess.py
@@ -5,16 +5,19 @@ from uepyscripts import logger
 
 def run_subprocess(cmd: list[str], log_output: bool = True) -> int:
     """Run a subprocess with the given command and return the Popen object."""
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, bufsize=1, universal_newlines=True)
-
-    assert process.stdout is not None
-    assert process.stderr is not None
-
     if log_output:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, bufsize=1, universal_newlines=True)
+
+        assert process.stdout is not None
+        assert process.stderr is not None
+        
         for line in process.stdout:
             line = line.strip()
             if line:
                 logger.info(line)
 
-    process.wait()
-    return process.returncode
+        process.wait()
+        return process.returncode
+    else:
+        subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        return 0
