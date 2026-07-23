10848f14e4231834b2bcd67d673f95811a4b2a03
Michael Delva <michael.delva@fishingcactus.com>
2026-07-24 00:29:26 +0200

    Created scripts to push / pull engine / game binaries

---
+++ src/uepyscripts/tools/ue/push_binaries.py
@@ -0,0 +1,26 @@
+import sys
+
+from uepyscripts.context import engine, project
+
+def main() -> None:
+    print("Compiling Unreal Editor...")
+    if (
+        engine.build(
+            [
+                f"{project.project_name}Editor",
+                "Win64",
+                "Development",
+                f"-project={project.uproject_path}",
+                "-WaitMutex",
+                "-FromMsBuild",
+                f"-log={project.root_folder}/Saved/Logs/Compile_Editor_Development_Win64.log",
+            ]
+        )
+        != 0
+    ):
+        print("Failed to compile Unreal Editor.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
